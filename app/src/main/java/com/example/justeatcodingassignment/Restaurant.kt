378591d473f9fb8fcc6bb7875d31fa957ba00557
DirieD <diriedudez@gmail.com>
2025-04-01 14:35:20 +0100

    Created API Service, data class to hold relevant data and Retrofit Client.

---
+++ app/src/main/java/com/example/justeatcodingassignment/Restaurant.kt
@@ -0,0 +1,24 @@
+package com.example.justeatcodingassignment
+
+data class RestaurantResponse(
+    val restaurants: List<Restaurant>, val resultCount: Int? = null, val area: String? = null
+)
+
+data class Restaurant(
+    val name: String?,
+    val cuisineDetail: List<CuisineDetail>?,
+    val rating: Rating?,
+    val address: Address?
+)
+
+data class CuisineDetail(
+    val name: String?
+)
+
+data class Address(
+    val city: String?, val firstLine: String?, val postcode: String?
+)
+
+data class Rating(
+    val count: Int?, val average: Double?, val starRating: Double?
+)
